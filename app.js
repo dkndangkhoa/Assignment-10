f81f29cbcefe7346404fd6579786201b894686c1
DESKTOP-238LMAC\Khoa <k.nguyendangkhoa@student.fontys.nl>
2023-12-14 13:53:09 +0100

    DKN update Assignment 10

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+// Passport configuration
+7
+require('./passport-config')(passport);
+const app = express();
+const routes = require('./routes/routes'); // Combined routes file
+// Set view engine
+app.set('view engine', 'ejs');
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+// Express session
+app.use(session({
+secret: 'secret',
+resave: false,
+saveUninitialized: false
+}));
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// Connect flash
+app.use(flash());
+// Global variables for flash messages
+app.use((req, res, next) => {
+res.locals.success_msg = req.flash('success_msg');
+res.locals.error_msg = req.flash('error_msg');
+res.locals.error = req.flash('error');
+next();
+});
+// Routes
+app.use('/', routes);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
